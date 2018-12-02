5d6c91e6c4c5a3ddb266bfe65a4435f4131a82d1
Chetan Patil <csp567@gmail.com>
2018-12-02 21:45:35 +0530

    Updated directory structure to deploy code on hosting server

--- resources/views/includes/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-	<div class="pull-right hidden-xs">
-		<b>Version</b> 2.4.0
-	</div>
-	<strong>Copyright &copy; 2016-<?php echo date("Y"); ?> <a target="_blank" href="http://www.chetanpatil.co.in">Chetan Patil</a>.</strong> All rights reserved.
-</footer>
