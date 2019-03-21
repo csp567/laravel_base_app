a70a9fa3cd0196b73e4fca5cb093e931dde9193f
Chetan Patil <csp567@gmail.com>
2019-03-21 18:36:13 +0530

    Fixes redirection issue Updated database to MariaDB

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -19,7 +19,7 @@
 	});
 
 	Auth::routes();
-
+	Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 	Route::get('/home', 'HomeController@index')->name('home');
 
 	Route::get('/profile', function(){
